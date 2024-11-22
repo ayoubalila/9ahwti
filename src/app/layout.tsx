a087dc5527d9aa5d7427167213fac82cc6617a62
Ayoub Alila <ayoub.alila@supcom.tn>
2024-11-22 16:22:40 +0100

    Second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,26 @@ children: React.ReactNode;
 return (
 <html lang="en">
 <body className={inter. className}>
-<header>
+<header className = "mb-16">
+     <div className="flex justify-between max-w-2xl mx-auto px-4 py-4">
+     <Link href={'/'} className = "inline-flex gap-1 items-center">
+     <FontAwesomeIcon className = "h-8" icon = {faMugHot} / >
+     <span className = "mt-2" >9ahwty</span>
+     </Link>
+     <nav className="mt-2 flex gap-6 items-center">
+     <Link href="/about">About</Link>
+     <Link href="/about">FAQ</Link>
+     <Link href="/about">Contact</Link>
+        <div className="flex gap-4">
+          <button className="border-2 rounded-full px-4 py-2 ml-4">Login</button>
+          <button className="bg-yellow-300 rounded-full px-4 py-2">Sign up</button>
+        </div>
+     </nav>
 
-<Link href={'/'}>
-     <FontAwesomeIcon icon = {faMugHot} / >
-     Buy Me a Coffee
-</Link>
+
+     </div>
+
+
 </header>
 {children}
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Tous les fichiers JavaScript/TypeScript/React dans src/
+    "./app/**/*.{js,ts,jsx,tsx}", // Tous les fichiers dans app/ (structure suggérée par Next.js 13+)
+    "./public/**/*.html" // Fichiers HTML statiques dans public/
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
